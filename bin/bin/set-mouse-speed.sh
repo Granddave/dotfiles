6398f8a01edacf9282b321d36827d5573de96165
David Isaksson <davidisaksson93@gmail.com>
2020-05-26 21:09:31 +0200

    Fix typo

--- bin/bin/set-mouse-speed.sh
+++ bin/bin/set-mouse-speed.sh
@@ -5,7 +5,7 @@
 # Sets the mouse speed with xinput for a specified device ID.
 
 if [ "$#" -ne 2 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-    echo "Usage:    $0 <Acceleration> <Device IDs"
+    echo "Usage:    $0 <Acceleration> <Device IDs>"
     echo "Example:  $0 -0.5 13 14"
     echo ""
     echo "Run 'xinput --list' to figure out the IDs of the mouse that you want to configure."
