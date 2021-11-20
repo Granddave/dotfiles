44c067240762c426d496c2fa3e48881b68c71ba0
David Isaksson <davidisaksson93@gmail.com>
2021-11-20 15:20:56 +0100

    install: Add question before install and fix shellcheck warnings

--- install.sh
+++ install.sh
@@ -1,40 +1,52 @@
 #!/usr/bin/env bash
 
-read -p "* Install apt packages? [Y/n] " RESPONSE
+set -e
+
+read -rp "* Install apt packages? [Y/n] " RESPONSE
 if [[ "${RESPONSE:-Y}" =~ ^[yY]$ ]]; then
-    xargs sudo apt-get install < apt-packages.txt
+    xargs -a apt-packages.txt sudo apt-get install
 fi
 
-read -p "* Install oh-my-zsh? [Y/n] " zsh
-zsh=${zsh:-Y}
-if [[ $zsh =~ ^[yY]$ ]]; then
+read -rp "* Install oh-my-zsh? [Y/n] " RESPONSE
+if [[ "${RESPONSE:-Y}" =~ ^[yY]$ ]]; then
     echo "* Installing oh-my-zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    chsh -s /bin/zsh
+    if [ ! -d "$HOME/.oh-my-zsh" ]; then
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+        chsh -s /bin/zsh
 
-    git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
-fi
-
-if [[ ! -d $HOME/.vim/autoload/plug.vim ]]; then
-    echo "* Installing vim-plug"
-    curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
-echo "* Starting to stow dotfiles..."
-dirs="alacritty bin redshift tmux vim xmodmap zsh"
-for dir in $dirs;
-do
-    if [[ -d $dir ]]; then
-        echo "* Stowing $dir"
-        stow $dir
+        git clone https://github.com/zsh-users/zsh-autosuggestions.git "$HOME/.oh-my-zsh/plugins/zsh-autosuggestions"
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting"
+    else
+        echo "oh-my-zsh already exists"
     fi
-done
+fi
 
-read -p "* Enabling weather service? [Y/n] " RESPONSE
+read -rp "* Install vim-plug? [Y/n] " RESPONSE
 if [[ "${RESPONSE:-Y}" =~ ^[yY]$ ]]; then
-    if [ ! -e $HOME/.config/systemd/user/weather.service ]; then
+    echo "* Installing vim-plug"
+    if [ ! -d $HOME/.vim/autoload/plug.vim ]; then
+        curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    else
+        echo "vim-plug already exists"
+    fi
+fi
+
+read -rp "* Stow directories? [Y/n] " RESPONSE
+if [[ "${RESPONSE:-Y}" =~ ^[yY]$ ]]; then
+    echo "* Starting to stow dotfiles..."
+    DIRS="alacritty bin git tmux vim xmodmap zsh"
+    for DIR in DIRS; do
+        if [ -d $DIR ]; then
+            echo "* Stowing $DIR"
+            stow "$DIR"
+        fi
+    done
+fi
+
+read -rp "* Enabling weather service? [Y/n] " RESPONSE
+if [[ "${RESPONSE:-Y}" =~ ^[yY]$ ]]; then
+    if [ ! -e "$HOME/.config/systemd/user/weather.service" ]; then
         echo "! Tmux directory is not stowed"
     else
         systemctl --user daemon-reload
