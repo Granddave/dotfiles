a448891845a0838814a170641bbeb1f2ffe4247b
David Isaksson <davidisaksson93@gmail.com>
2022-07-25 07:49:48 +0200

    snippets: Add Snippet utility

---
+++ bin/bin/snippets.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+SNIPS=${HOME}/sync/Snippets
+FILE=$(find "${SNIPS}" -type f -printf "%f\n" | /usr/bin/rofi -dmenu)
+SNIPPET="${SNIPS}/${FILE}"
+
+if [ -f "${SNIPPET}" ]; then
+    if [ -x "${SNIPPET}" ]; then
+        DATA=$("${SNIPPET}")
+    else
+        DATA=$(head --bytes=-1 "${SNIPPET}")
+    fi
+
+    printf "$DATA" | xsel -p -i
+    printf "$DATA" | xsel -b -i
+    xdotool key shift+Insert
+fi
