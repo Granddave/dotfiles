c935de17731cb1e9143f010ca858436f323d9ff6
David Isaksson <davidisaksson93@gmail.com>
2022-08-25 22:44:30 +0200

    install: Add possibility to do a CLI-only install

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install python3-pip
+/usr/bin/env python3 -m pip install ansible
+echo "$PATH" | grep "$HOME/.local/bin" || ( echo 'PATH=$PATH:$HOME/.local/bin' >> $HOME/.bashrc )
