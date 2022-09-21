b638b5328f4445e77bddc85a9edf3b3264e7d3ea
David Isaksson <davidisaksson93@gmail.com>
2022-09-21 19:54:55 +0200

    vim: Hide lualine when Goyo
    This solution is not super nice. Needed to toggle Goyo three times to
    get rid of the tabline in the top...

--- vim/.config/nvim/lua/plugins/goyo.lua
+++ vim/.config/nvim/lua/plugins/goyo.lua
@@ -1,1 +1,6 @@
-vim.keymap.set("n", "<Leader>go", "<Cmd>Goyo<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>go", "<Cmd>Goyo<CR><Cmd>Goyo<CR><Cmd>Goyo<CR>", { noremap = true, silent = true })
+
+vim.cmd([[
+autocmd! User GoyoEnter nested :luado require('lualine').hide({unhide=false})
+autocmd! User GoyoLeave nested :luado require('lualine').hide({unhide=true})
+]])
