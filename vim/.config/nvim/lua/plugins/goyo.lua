2f8a4aff9b38749e839301848009e2e37f070fcb
David Isaksson <davidisaksson93@gmail.com>
2022-09-24 22:10:42 +0200

    vim: Hide Tmux status bar when in Goyo

--- vim/.config/nvim/lua/plugins/goyo.lua
+++ vim/.config/nvim/lua/plugins/goyo.lua
@@ -2,6 +2,9 @@ vim.keymap.set("n", "<Leader>go", "<Cmd>Goyo<CR><Cmd>Goyo<CR><Cmd>Goyo<CR>", { n
 
 vim.cmd([[
 function! s:goyo_enter()
+  if executable('tmux') && strlen($TMUX)
+    silent !tmux set status off
+  endif
   luado require('lualine').hide()
   Limelight
   set cursorline!
@@ -9,6 +12,9 @@ endfunction
 autocmd! User GoyoEnter nested call <SID>goyo_enter()
 
 function! s:goyo_leave()
+  if executable('tmux') && strlen($TMUX)
+    silent !tmux set status on
+  endif
   luado require('lualine').hide({unhide=true})
   Limelight!
   set cursorline
