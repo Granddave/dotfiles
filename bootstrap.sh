66bd43b715c6b9c8965c403135a4bfc8ec56c79a
David Isaksson <davidisaksson93@gmail.com>
2022-09-30 22:52:42 +0200

    install: Wrap variable in quotes

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install python3-pip
 /usr/bin/env python3 -m pip install ansible
-echo "$PATH" | grep "$HOME/.local/bin" || ( echo 'PATH=$PATH:$HOME/.local/bin' >> $HOME/.bashrc )
+echo "$PATH" | grep "$HOME/.local/bin" || ( echo 'PATH=$PATH:$HOME/.local/bin' >> "$HOME/.bashrc" )
