911c731d93983066457735165c3a21b64734b674
David Isaksson <davidisaksson93@gmail.com>
2022-09-30 20:04:54 +0200

    vim: Fix checkboxToggle() once and for all
    This has been bothering me for quite some time. I couldn't get the
    multiline visual toggle to work. This change fixed it.

--- vim/.config/nvim/lua/plugins/vim-checkbox.lua
+++ vim/.config/nvim/lua/plugins/vim-checkbox.lua
@@ -1,1 +1,1 @@
-vim.keymap.set("", "<Leader>x", "<Cmd>call checkbox#ToggleCB()<CR>", { noremap = true, silent = true })
+vim.keymap.set("", "<Leader>x", ":call checkbox#ToggleCB()<CR>", { noremap = true, silent = true })
