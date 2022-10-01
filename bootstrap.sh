7d541fb8acc7cc3b2d75c6433cee22c70bc3dea9
David Isaksson <davidisaksson93@gmail.com>
2022-10-01 12:30:47 +0200

    install: Disable warning about not expanding variables

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,5 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install python3-pip
 /usr/bin/env python3 -m pip install ansible
+# shellcheck disable=SC2016
 echo "$PATH" | grep "$HOME/.local/bin" || ( echo 'PATH=$PATH:$HOME/.local/bin' >> "$HOME/.bashrc" )
