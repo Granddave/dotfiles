c5b8018e5c98e9372073ae8d95a72e25ad3f29c6
David Isaksson <davidisaksson93@gmail.com>
2022-10-06 21:51:32 +0200

    vim: Port Goyo autocmd to lua

--- vim/.config/nvim/lua/plugins/goyo.lua
+++ vim/.config/nvim/lua/plugins/goyo.lua
@@ -1,23 +1,26 @@
 vim.keymap.set("n", "<Leader>go", "<Cmd>Goyo<CR><Cmd>Goyo<CR><Cmd>Goyo<CR>", { noremap = true, silent = true })
 
-vim.cmd([[
-function! s:goyo_enter()
-  if executable('tmux') && strlen($TMUX)
-    silent !tmux set status off
-  endif
-  luado require('lualine').hide()
-  Limelight
-  set cursorline!
-endfunction
-autocmd! User GoyoEnter nested call <SID>goyo_enter()
-
-function! s:goyo_leave()
-  if executable('tmux') && strlen($TMUX)
-    silent !tmux set status on
-  endif
-  luado require('lualine').hide({unhide=true})
-  Limelight!
-  set cursorline
-endfunction
-autocmd! User GoyoLeave nested call <SID>goyo_leave()
-]])
+vim.api.nvim_create_autocmd("User", {
+  pattern = "GoyoEnter",
+  callback = function()
+    if vim.fn.executable('tmux') == 1 and os.getenv('TMUX') then
+      io.popen('tmux set status off')
+    end
+    require('lualine').hide()
+    vim.cmd("Limelight")
+    vim.opt.cursorline = false
+    vim.opt.showmode = false
+  end,
+})
+vim.api.nvim_create_autocmd("User", {
+  pattern = "GoyoLeave",
+  callback = function()
+    if vim.fn.executable('tmux') == 1 and os.getenv('TMUX') then
+      io.popen('tmux set status on')
+    end
+    require('lualine').hide({ unhide = true })
+    vim.cmd("Limelight!")
+    vim.opt.cursorline = true
+    vim.opt.showmode = true
+  end,
+})
