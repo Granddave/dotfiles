2eaec2c0098f7ff9d47dfee042a8bdae1be4ebd3
David Isaksson <davidisaksson93@gmail.com>
2022-10-18 21:45:00 +0200

    vim: Remove disabling of font icons for nvim-tree

--- vim/.config/nvim/lua/plugins/nvim-tree.lua
+++ vim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,14 +1,4 @@
 require("nvim-tree").setup({
-  renderer = {
-    icons = {
-      show = {
-        file = false,
-        folder = false,
-        folder_arrow = false,
-        git = false,
-      }
-    }
-  }
 })
 
 vim.keymap.set("n", "<Leader>e", "<Cmd>NvimTreeToggle<CR>", { noremap = true, silent = true })
