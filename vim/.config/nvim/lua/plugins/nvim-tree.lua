588c34429ae158c03654741007212f49e6973468
David Isaksson <davidisaksson93@gmail.com>
2022-11-02 20:51:28 +0100

    vim: Experiment with nvim-tree configs

--- vim/.config/nvim/lua/plugins/nvim-tree.lua
+++ vim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,4 +1,16 @@
 require("nvim-tree").setup({
+  view = {
+    adaptive_size = true,
+    width = 40,
+  },
+  renderer = {
+    icons = {
+      git_placement = "after",
+    },
+  },
+  git = {
+    ignore = false,
+  },
 })
 
 vim.keymap.set("n", "<Leader>e", "<Cmd>NvimTreeToggle<CR>", { noremap = true, silent = true })
