c9b58a20c2fa90cc1ea55bd83d6da9696f9b8300
David Isaksson <davidisaksson93@gmail.com>
2022-11-18 23:00:40 +0100

    vim: Disable inline diagnostics and require hover for pop-up
    Thanks to @wookayin
    
    https://old.reddit.com/r/neovim/comments/mqspfo/is_there_any_way_to_hide_the_diagnostic_from/hhop19z/
    https://github.com/wookayin/dotfiles/commit/594172460db514df3b3e8dc4726b4e052ba59e65

--- vim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ vim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -120,3 +120,22 @@ vim.keymap.set("n", "<Leader>cpp",
   end,
   { noremap = true, silent = true }
 )
+
+-- Disable inline diagnostics and require hover for pop-up window
+vim.diagnostic.config({
+  virtual_text = false,
+})
+vim.api.nvim_create_autocmd("CursorHold", {
+  pattern = "*",
+  group = vim.api.nvim_create_augroup("LSPDiagnosticsOnHover", {}),
+  callback = function()
+    local current_cursor = vim.api.nvim_win_get_cursor(0)
+    local last_popup_cursor = vim.w.lsp_diagnostics_last_cursor or { nil, nil }
+    -- Show the pop-up diagnostics window,
+    -- but only once for the current cursor location (unless moved afterwards).
+    if not (current_cursor[1] == last_popup_cursor[1] and current_cursor[2] == last_popup_cursor[2]) then
+      vim.w.lsp_diagnostics_last_cursor = current_cursor
+      vim.diagnostic.open_float(0, { scope = "cursor" })
+    end
+  end
+})
