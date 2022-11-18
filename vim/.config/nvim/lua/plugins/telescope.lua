f6fa3170930193c6b353b70219201665bc0c0f78
David Isaksson <davidisaksson93@gmail.com>
2022-11-18 22:25:10 +0100

    vim: Improve Telescope UI, revert to grep_string, add filetype select

--- vim/.config/nvim/lua/plugins/telescope.lua
+++ vim/.config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,11 @@
 require('telescope').setup({
   defaults = {
-    winblend = 5
+    winblend = 5,
+    layout_config = {
+      horizontal = {
+        width = 0.95
+      },
+    },
   }
 })
 require('telescope').load_extension('fzf')
@@ -9,6 +14,8 @@ require('telescope').load_extension('harpoon')
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 local telescope = require('telescope.builtin')
+local themes = require('telescope.themes')
+
 map("n", "<Leader>ff",
   function()
     telescope.find_files({
@@ -25,7 +32,16 @@ map("n", "<Leader>fnv",
   end, opts
 )
 map("n", "<Leader>fg", function() telescope.git_files() end, opts)
-map("n", "<Leader>fr", function() telescope.grep_string({ search = '' }) end, opts)
+map("n", "<Leader>fr", function() telescope.live_grep() end, opts)
 map("n", "<Leader>fb", function() telescope.buffers() end, opts)
 map("n", "<Leader>fh", function() telescope.help_tags() end, opts)
-map("n", "<Leader>ss", function() telescope.spell_suggest() end, opts)
+map("n", "<Leader>ss",
+  function()
+    telescope.spell_suggest(themes.get_cursor({
+      layout_config = {
+        height = 20
+      },
+    }))
+  end, opts
+)
+map("n", "<Leader>sft", function() telescope.filetypes(themes.get_dropdown({})) end, opts)
