860601f19523ed0fe7bb701846a9f714c52ae087
David Isaksson <davidisaksson93@gmail.com>
2022-11-22 22:48:39 +0100

    vim: Add some gitsigns mappings

--- vim/.config/nvim/lua/plugins/gitsigns.lua
+++ vim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,3 +1,16 @@
-require('gitsigns').setup({
+local map = vim.keymap.set
+local opts = { noremap = false, silent = true }
+local gitsigns = require('gitsigns')
+
+gitsigns.setup({
   numhl = true,
+  on_attach = function(bufnr)
+    -- TODO: Add range based staging and reset
+    map("v", "<Leader>ga", function() gitsigns.stage_hunk() end, opts)
+    map("n", "<Leader>gp", function() gitsigns.preview_hunk() end, opts)
+    map("n", "<Leader>gu", function() gitsigns.undo_stage_hunk() end, opts)
+    map("n", "<Leader>gd", function() gitsigns.diffthis() end, opts)
+    map("n", "<Leader>gs", function() gitsigns.show() end, opts)
+    map("n", "<Leader>gb", function() gitsigns.blame_line() end, opts)
+  end
 })
