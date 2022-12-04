59908eaf76fa850f72eb2ade2f7c3e0d7c2a0601
David Isaksson <davidisaksson93@gmail.com>
2022-12-04 11:24:27 +0100

    vim: Simplify telescope fzf installation

--- vim/.config/nvim/lua/custom/plugins.lua
+++ vim/.config/nvim/lua/custom/plugins.lua
@@ -24,7 +24,7 @@ return require('packer').startup(function()
   }
   use {
     'nvim-telescope/telescope-fzf-native.nvim',
-    run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    run = 'make'
   }
   use 'ThePrimeagen/harpoon'
   use {
