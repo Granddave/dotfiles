d7ec09d0699c2a62b5fc5662946679b12b97444b
David Isaksson <davidisaksson93@gmail.com>
2023-01-03 19:08:00 +0100

    setup: Format bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,4 @@ sudo apt-get -y upgrade
 sudo apt-get -y install python3-pip
 /usr/bin/env python3 -m pip install ansible
 # shellcheck disable=SC2016
-echo "$PATH" | grep "$HOME/.local/bin" || ( echo 'PATH=$PATH:$HOME/.local/bin' >> "$HOME/.bashrc" )
+echo "$PATH" | grep "$HOME/.local/bin" || (echo 'PATH=$PATH:$HOME/.local/bin' >> "$HOME/.bashrc")
