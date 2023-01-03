f2754a7016339a9208bec0d227534389cea8eca3
David Isaksson <davidisaksson93@gmail.com>
2023-01-03 19:06:05 +0100

    vim: Add more null-ls sources and add flag to shfmt regarding spaces

--- vim/.config/nvim/after/plugin/null-ls.lua
+++ vim/.config/nvim/after/plugin/null-ls.lua
@@ -1,16 +1,26 @@
-local ok, null_ls = pcall(require, "null-ls")
-if not ok then
+if not HAS("null-ls") then
   return
 end
 
+local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
-    --null_ls.builtins.formatting.stylua,
+    -- Shell
     null_ls.builtins.formatting.shfmt.with({
       filetypes = { "sh", "bash" },
-      extra_args = { "-i", "4", "-ci" },
+      extra_args = { "-i", "4", "--case-indent", "--space-redirects" },
     }),
+
+    -- Python
     null_ls.builtins.formatting.black,
     null_ls.builtins.diagnostics.pylint,
+    null_ls.builtins.diagnostics.mypy,
+    null_ls.builtins.diagnostics.flake8,
+
+    -- C++
+    null_ls.builtins.diagnostics.cmake_lint,
+
+    -- Yaml
+    null_ls.builtins.diagnostics.yamllint,
   },
 })
