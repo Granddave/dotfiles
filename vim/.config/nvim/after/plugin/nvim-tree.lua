9210b38add86f7dd45e8c41e1c505fc40c4779f5
David Isaksson <davidisaksson93@gmail.com>
2023-03-13 20:49:51 +0100

    vim: Update nvim-tree to use on_attach and not deprecated mappings

--- vim/.config/nvim/after/plugin/nvim-tree.lua
+++ vim/.config/nvim/after/plugin/nvim-tree.lua
@@ -7,13 +7,6 @@ nvim_tree.setup({
   view = {
     adaptive_size = false,
     width = 35,
-    mappings = {
-      list = {
-        { key = "<C-e>", action = "" },
-        { key = "h", action = "close_node" },
-        { key = "l", action = "open" },
-      },
-    },
   },
   actions = {
     open_file = {
@@ -31,6 +24,25 @@ nvim_tree.setup({
   diagnostics = {
     enable = true,
   },
+  on_attach = function(bufnr)
+    local api = require('nvim-tree.api')
+    api.config.mappings.default_on_attach(bufnr)
+
+    local opts = function(desc)
+      return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+    local remove = function(mode, binding)
+      -- The dummy set before del is done for safety, in case a default mapping does not exist.
+      vim.keymap.set(mode, binding, '', { buffer = bufnr })
+      vim.keymap.del(mode, binding, { buffer = bufnr })
+    end
+    remove("n", "<C-e>") -- Scroll
+
+    vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+    vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+    remove("n", "C")
+    vim.keymap.set('n', 'C', api.tree.change_root_to_node, opts('CD'))
+  end
 })
 
 vim.keymap.set("n", "<Leader>e", "<Cmd>NvimTreeToggle<CR>", { noremap = true, silent = true })
