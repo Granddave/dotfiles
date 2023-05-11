04268d11d7377340d02bdb0363f633885aad9076
David Isaksson <davidisaksson93@gmail.com>
2023-05-11 18:16:24 +0200

    vim: Fix error on missing diagnostics

--- vim/.config/nvim/after/plugin/nvim-lspconfig.lua
+++ vim/.config/nvim/after/plugin/nvim-lspconfig.lua
@@ -201,7 +201,7 @@ vim.diagnostic.config({
         "%s (%s) [%s]",
         diagnostic.message,
         diagnostic.source,
-        diagnostic.code or diagnostic.user_data.lsp.code
+        diagnostic.code or diagnostic.user_data.lsp.code or ""
       )
     end,
   },
