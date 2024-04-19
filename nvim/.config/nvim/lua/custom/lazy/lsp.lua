f9dfb6478a7447928343d935fa50c6088a51f667
David Isaksson <davidisaksson93@gmail.com>
2024-04-19 19:15:56 +0200

    nvim: Remove lua globals due to addition of neodev

--- nvim/.config/nvim/lua/custom/lazy/lsp.lua
+++ nvim/.config/nvim/lua/custom/lazy/lsp.lua
@@ -17,14 +17,6 @@ local servers = {
         runtime = {
           version = "LuaJIT",
         },
-        diagnostics = {
-          globals = {
-            "vim",
-            "use",
-            "print",
-            "require",
-          },
-        },
         workspace = {
           library = vim.api.nvim_get_runtime_file("", true),
           checkThirdParty = false,
