3a2277577f87bdfdb81b17116d9db8cd7c6b6a18
David Isaksson <davidisaksson93@gmail.com>
2024-12-28 12:20:38 +0100

    nvim: Make gruvbox background darker and add colorizer plugin

--- nvim/.config/nvim/lua/custom/lazy/gruvbox.lua
+++ nvim/.config/nvim/lua/custom/lazy/gruvbox.lua
@@ -4,6 +4,9 @@ return {
   init = function()
     require("gruvbox").setup({
       contrast = "hard",
+      palette_overrides = {
+        dark0_hard = "#1b1b1b",
+      }
     })
     vim.cmd("colorscheme gruvbox")
   end
