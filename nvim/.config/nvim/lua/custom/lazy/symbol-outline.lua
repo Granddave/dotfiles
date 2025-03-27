3b6f2d8cfb505b128f6ab1a482e0f254d8a91834
David Isaksson <davidisaksson93@gmail.com>
2025-03-27 10:21:23 +0100

    nvim: Add outline plugin

---
+++ nvim/.config/nvim/lua/custom/lazy/symbol-outline.lua
@@ -0,0 +1,9 @@
+return {
+  "hedyhli/outline.nvim",
+  lazy = true,
+  cmd = { "Outline", "OutlineOpen" },
+  keys = {
+    { "<leader>ol", "<cmd>Outline<CR>", desc = "Toggle outline" },
+  },
+  opts = {},
+}
