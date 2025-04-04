760268c7f5447b634be7855a8b44930bf8b5dc16
David Isaksson <davidisaksson93@gmail.com>
2025-04-04 07:52:50 +0200

    nvim: Add keybindings for git operations

--- nvim/.config/nvim/lua/custom/lazy/git.lua
+++ nvim/.config/nvim/lua/custom/lazy/git.lua
@@ -5,19 +5,42 @@ return {
     current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
     current_line_blame_opts = {
       virt_text = true,
-      virt_text_pos = 'eol',   -- 'eol' | 'overlay' | 'right_align'
+      virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
       delay = 100,
     },
     on_attach = function(bufnr)
       _ = bufnr
-      -- TODO: Add range based staging and reset
       local map = vim.keymap.set
       local opts = { noremap = false, silent = true }
       local gitsigns = require("gitsigns")
-      map("n", "<Leader>ga", function() gitsigns.stage_hunk() end, opts)
-      map("n", "<Leader>gp", function() gitsigns.preview_hunk() end, opts)
-      map("n", "<Leader>gu", function() gitsigns.undo_stage_hunk() end, opts)
-      map("n", "<Leader>gd", function() gitsigns.diffthis() end, opts)
+
+      -- Navigation
+      map('n', '<leader>gn', function()
+        if vim.wo.diff then
+          vim.cmd.normal({ ']c', bang = true })
+        else
+          gitsigns.nav_hunk('next')
+        end
+      end)
+      map('n', '<leader>gp', function()
+        if vim.wo.diff then
+          vim.cmd.normal({ '[c', bang = true })
+        else
+          gitsigns.nav_hunk('prev')
+        end
+      end)
+
+      -- Staging
+      map('n', '<leader>ga', gitsigns.stage_hunk)
+      map('v', '<leader>ga', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+      map('n', '<leader>gu', gitsigns.undo_stage_hunk)
+      map('n', '<leader>gA', gitsigns.reset_hunk)
+      map('v', '<leader>gA', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+
+      map('n', '<leader>gdp', gitsigns.preview_hunk)
+      map("n", "<Leader>gd", gitsigns.diffthis, opts)
+      map("n", "<Leader>do", vim.cmd("windo diffoff"), opts)
+      map("n", "<Leader>gD", function() gitsigns.diffthis('~') end, opts)
       map("n", "<Leader>gs", function() gitsigns.show() end, opts)
       map("n", "<Leader>gb", function() gitsigns.blame_line() end, opts)
     end
