a1cd93bb5bef9cb424069e3936cff599c7ec57c8
David Isaksson <davidisaksson93@gmail.com>
2025-07-13 10:20:19 +0200

    nvim: Redo Gitsigns keymaps

--- nvim/.config/nvim/lua/custom/lazy/git.lua
+++ nvim/.config/nvim/lua/custom/lazy/git.lua
@@ -10,38 +10,71 @@ return {
     },
     on_attach = function(bufnr)
       _ = bufnr
-      local map = vim.keymap.set
-      local opts = { noremap = false, silent = true }
       local gitsigns = require("gitsigns")
 
-      -- Navigation
-      map("n", "<leader>gn", function()
-        if vim.wo.diff then
-          vim.cmd.normal({ "]c", bang = true })
-        else
-          gitsigns.nav_hunk("next")
-        end
-      end)
-      map("n", "<leader>gp", function()
-        if vim.wo.diff then
-          vim.cmd.normal({ "[c", bang = true })
-        else
-          gitsigns.nav_hunk("prev")
-        end
-      end)
+      require("which-key").add({
+        -- Navigation
+        {
+          "<leader>gn",
+          function()
+            if vim.wo.diff then
+              vim.cmd.normal({ "]c", bang = true })
+            else
+              gitsigns.nav_hunk("next")
+            end
+          end,
+          desc = "Git: Next Hunk",
+        },
+        {
+          "<leader>gp",
+          function()
+            if vim.wo.diff then
+              vim.cmd.normal({ "[c", bang = true })
+            else
+              gitsigns.nav_hunk("prev")
+            end
+          end,
+          desc = "Git: Previous Hunk",
+        },
 
-      -- Staging
-      map("n", "<leader>ga", gitsigns.stage_hunk)
-      map("v", "<leader>ga", function() gitsigns.stage_hunk { vim.fn.line("."), vim.fn.line("v") } end)
-      map("n", "<leader>gA", gitsigns.reset_hunk)
-      map("v", "<leader>gA", function() gitsigns.reset_hunk { vim.fn.line("."), vim.fn.line("v") } end)
+        -- Staging
+        { "<leader>ga",  gitsigns.stage_hunk,                   desc = "Git: Stage/Unstage Hunk", mode = { "n" } },
+        {
+          "<leader>ga",
+          function() gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end,
+          desc = "Git: Stage Hunk/Unstage (Visual)",
+          mode = { "v" }
+        },
+        {
+          "<leader>gR",
+          function()
+            local confirm = vim.fn.confirm("Are you sure you want to reset this hunk?", "&Yes\n&No", 2)
+            if confirm == 1 then
+              gitsigns.reset_hunk()
+            end
+          end,
+          desc = "Git: Reset Hunk",
+          mode = { "n" }
+        },
+        {
+          "<leader>gR",
+          function()
+            local confirm = vim.fn.confirm("Are you sure you want to reset the selection?", "&Yes\n&No", 2)
+            if confirm == 1 then
+              gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+            end
+          end,
+          desc = "Git: Reset Hunk (Visual)",
+          mode = { "v" }
+        },
 
-      map("n", "<leader>gdp", gitsigns.preview_hunk)
-      map("n", "<Leader>gd", gitsigns.diffthis, opts)
-      map("n", "<Leader>do", function() vim.cmd("windo diffoff") end, opts)
-      map("n", "<Leader>gD", function() gitsigns.diffthis("~") end, opts)
-      map("n", "<Leader>gs", function() gitsigns.show() end, opts)
-      map("n", "<Leader>gb", function() gitsigns.blame_line() end, opts)
+        { "<leader>ghp", gitsigns.preview_hunk,                 desc = "Git: Preview Hunk" },
+        { "<Leader>gd",  gitsigns.diffthis,                     desc = "Git: Diff This (Index)" },
+        { "<Leader>gD",  function() gitsigns.diffthis("~") end, desc = "Git: Diff This (HEAD)" },
+        { "<Leader>gs",  gitsigns.show,                         desc = "Git: Show" },
+        { "<Leader>gb",  gitsigns.blame_line,                   desc = "Git: Blame Line" },
+        { "<Leader>gB",  gitsigns.blame,                        desc = "Git: Blame File" },
+      })
     end
   },
 }
