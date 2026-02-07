dc35e6fc928f67a1ca916a136e36597732459156
David Isaksson <davidisaksson93@gmail.com>
2026-02-07 14:50:36 +0100

    nvim: Add CodeDiff.nvim
    Not perfect Git diff tool since it seemingly cannot differentiate
    between index and HEAD, so I'll keep gitsigns diff around.

--- nvim/.config/nvim/lua/custom/lazy/git.lua
+++ nvim/.config/nvim/lua/custom/lazy/git.lua
@@ -1,81 +1,91 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  opts = {
-    numhl = true,
-    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-    current_line_blame_opts = {
-      virt_text = true,
-      virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-      delay = 100,
+  {
+    "esmuellert/codediff.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    cmd = "CodeDiff",
+    keys = {
+      { "<Leader>gd", [[<Cmd>CodeDiff file HEAD<CR>]], desc = "Git: Diff This (Index)" },
     },
-    sign_priority = 50,
-    on_attach = function(bufnr)
-      _ = bufnr
-      local gitsigns = require("gitsigns")
-
-      require("which-key").add({
-        -- Navigation
-        {
-          "<leader>gn",
-          function()
-            if vim.wo.diff then
-              vim.cmd.normal({ "]c", bang = true })
-            else
-              gitsigns.nav_hunk("next")
-            end
-          end,
-          desc = "Git: Next Hunk",
-        },
-        {
-          "<leader>gp",
-          function()
-            if vim.wo.diff then
-              vim.cmd.normal({ "[c", bang = true })
-            else
-              gitsigns.nav_hunk("prev")
-            end
-          end,
-          desc = "Git: Previous Hunk",
-        },
-
-        -- Staging
-        { "<leader>ga",  gitsigns.stage_hunk,                   desc = "Git: Stage/Unstage Hunk", mode = { "n" } },
-        {
-          "<leader>ga",
-          function() gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end,
-          desc = "Git: Stage Hunk/Unstage (Visual)",
-          mode = { "v" }
-        },
-        {
-          "<leader>gR",
-          function()
-            local confirm = vim.fn.confirm("Are you sure you want to reset this hunk?", "&Yes\n&No", 2)
-            if confirm == 1 then
-              gitsigns.reset_hunk()
-            end
-          end,
-          desc = "Git: Reset Hunk",
-          mode = { "n" }
-        },
-        {
-          "<leader>gR",
-          function()
-            local confirm = vim.fn.confirm("Are you sure you want to reset the selection?", "&Yes\n&No", 2)
-            if confirm == 1 then
-              gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-            end
-          end,
-          desc = "Git: Reset Hunk (Visual)",
-          mode = { "v" }
-        },
-
-        { "<leader>ghp", gitsigns.preview_hunk,                 desc = "Git: Preview Hunk" },
-        { "<Leader>gd",  gitsigns.diffthis,                     desc = "Git: Diff This (Index)" },
-        { "<Leader>gD",  function() gitsigns.diffthis("~") end, desc = "Git: Diff This (HEAD)" },
-        { "<Leader>gs",  gitsigns.show,                         desc = "Git: Show" },
-        { "<Leader>gb",  gitsigns.blame_line,                   desc = "Git: Blame Line" },
-        { "<Leader>gB",  gitsigns.blame,                        desc = "Git: Blame File" },
-      })
-    end
   },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      numhl = true,
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 100,
+      },
+      sign_priority = 50,
+      on_attach = function(bufnr)
+        _ = bufnr
+        local gitsigns = require("gitsigns")
+
+        require("which-key").add({
+          -- Navigation
+          {
+            "<leader>gn",
+            function()
+              if vim.wo.diff then
+                vim.cmd.normal({ "]c", bang = true })
+              else
+                gitsigns.nav_hunk("next")
+              end
+            end,
+            desc = "Git: Next Hunk",
+          },
+          {
+            "<leader>gp",
+            function()
+              if vim.wo.diff then
+                vim.cmd.normal({ "[c", bang = true })
+              else
+                gitsigns.nav_hunk("prev")
+              end
+            end,
+            desc = "Git: Previous Hunk",
+          },
+
+          -- Staging
+          { "<leader>ga",  gitsigns.stage_hunk,                   desc = "Git: Stage/Unstage Hunk", mode = { "n" } },
+          {
+            "<leader>ga",
+            function() gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end,
+            desc = "Git: Stage Hunk/Unstage (Visual)",
+            mode = { "v" }
+          },
+          {
+            "<leader>gR",
+            function()
+              local confirm = vim.fn.confirm("Are you sure you want to reset this hunk?", "&Yes\n&No", 2)
+              if confirm == 1 then
+                gitsigns.reset_hunk()
+              end
+            end,
+            desc = "Git: Reset Hunk",
+            mode = { "n" }
+          },
+          {
+            "<leader>gR",
+            function()
+              local confirm = vim.fn.confirm("Are you sure you want to reset the selection?", "&Yes\n&No", 2)
+              if confirm == 1 then
+                gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+              end
+            end,
+            desc = "Git: Reset Hunk (Visual)",
+            mode = { "v" }
+          },
+
+          { "<leader>ghp", gitsigns.preview_hunk,                 desc = "Git: Preview Hunk" },
+          { "<Leader>gD",  gitsigns.diffthis,                     desc = "Git: Diff This (Index)" },
+          -- { "<Leader>gD",  function() gitsigns.diffthis("~") end, desc = "Git: Diff This (HEAD)" },
+          { "<Leader>gs",  gitsigns.show,                         desc = "Git: Show" },
+          { "<Leader>gb",  gitsigns.blame_line,                   desc = "Git: Blame Line" },
+          { "<Leader>gB",  gitsigns.blame,                        desc = "Git: Blame File" },
+        })
+      end
+    },
+  }
 }
