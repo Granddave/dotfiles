60b340af8cfbb6f4508122f47d863b1a77b1628c
David Isaksson <davidisaksson93@gmail.com>
2026-07-08 08:49:58 +0200

    fish: daily and weekly note functions

--- fish/.config/fish/conf.d/notes.fish
+++ fish/.config/fish/conf.d/notes.fish
@@ -1,7 +1,9 @@
 if test -d "$HOME/sync/Life"
     set -x NOTE_DIR "$HOME/sync/Life"
+    set -x DAILY_NOTE_DIR "$NOTE_DIR/Daily"
 else
     set -x NOTE_DIR "$HOME/Documents/Notes"
+    set -x DAILY_NOTE_DIR "$NOTE_DIR"
 end
 function notes
     cd $NOTE_DIR
@@ -9,6 +11,8 @@ function notes
 end
 function daily
     cd $NOTE_DIR
-    $EDITOR $NOTE_DIR/$(date +%Y-%m-%d).md
+    $EDITOR $DAILY_NOTE_DIR/$(date +%Y-%m-%d).md
 end
-
+function weekly
+  $EDITOR $DAILY_NOTE_DIR/$(date +%Y-W%V).md
+end
